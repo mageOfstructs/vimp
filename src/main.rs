e9a0b3382262d12b4e27990cc6b4e06fef8a452f
mageOfstructs <jason.puschnig@gmx.at>
2024-10-28 18:16:39 +0100

    mount reader

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use leptos::mount_to_body;
 use leptos::view;
+use vimp::components::Reader;
 
 fn main() {
-    mount_to_body(|| view! {test});
+    mount_to_body(|| {
+        view! {
+            <Reader/>
+        }
+    });
     println!("Hello, world!");
 }
